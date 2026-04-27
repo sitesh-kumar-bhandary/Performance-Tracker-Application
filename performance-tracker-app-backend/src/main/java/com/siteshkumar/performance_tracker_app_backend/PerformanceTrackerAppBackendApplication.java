afaa2a48ffc9fb552c0c7ec60c5f3b62c6fa384b
Sitesh01 <siteshisunique1998@gmail.com>
2026-04-27 23:41:10 +0530

    first commit

---
+++ performance-tracker-app-backend/src/main/java/com/siteshkumar/performance_tracker_app_backend/PerformanceTrackerAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.siteshkumar.performance_tracker_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformanceTrackerAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformanceTrackerAppBackendApplication.class, args);
+		System.out.println("This project has been developed by Sitesh Kumar");
+	}
+}
---
+++ performance-tracker-app-backend/src/test/java/com/siteshkumar/performance_tracker_app_backend/PerformanceTrackerAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siteshkumar.performance_tracker_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformanceTrackerAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
